6ef724f597199492d3ff8726b7870ac177709f16
Alejandro Meléndez Fernández <alexmelfer.amf@gmail.com>
2025-11-20 22:28:17 +0100

    Base creada(creo) faltan comentarios, no he puesto muchos

--- src/comments.py
+++ src/comments.py
@@ -1,2 +1,12 @@
-def porcentajes():
-    print("caca")
+import main
+import json
+import os
+from pathlib import Path
+import random
+import comments
+import percents
+
+def comentarios(c1,c2):
+    golpeador = percents.quienPega(c1,c2)
+    golpeado = c1 if golpeador == c2 else c2
+    print("")
--- src/main.py
+++ src/main.py
@@ -2,6 +2,8 @@ import json
 import os
 from pathlib import Path
 import time
+import comments
+import percents
 
 def main():
     base = Path(__file__).resolve().parent
@@ -16,25 +18,40 @@ def main():
         for p in personajes:
             print(f"{counter}.{p.capitalize()}")
             counter+=1
-        c1 = input("Escribe el nombre del luchador 1: ")
-        if c1.lower() not in personajes:
+        c1 = input("Escribe el nombre del luchador 1: ").lower()
+        if c1 not in personajes:
             while True:
                 print("Escribe el nombre bien, gilipollas, dale a enter y vuelve a intentarlo")
                 input("")
                 main()
-        c2 = input("Escribe el nombre del luchador 2: ")
-        if c2.lower() not in personajes:
+        c2 = input("Escribe el nombre del luchador 2: ").lower
+        if c2 not in personajes:
             while True:
                 print("Escribe el nombre bien, gilipollas, dale a enter y vuelve a intentarlo")
                 input("")
                 main()
-        # print("QUE EMPIECE LA BATALLA EN")
-        # print("3")
-        # time.sleep(1)
-        # print("2")
-        # time.sleep(1)
-        # print("1")
-        # time.sleep(1)
-        # os.system("cls")
+    porcentajeDeC1 = 0
+    porcentajeDeC2 = 0
+    vidasDeC1 = 3
+    vidasDeC2 = 3
+    agilizador = 0 #Mi idea es que las primeras 4 interacciones haya algunas funciones como la de "seMuere" que no compruebe ya que es una perdida de eficiencia
+    print("QUE EMPIECE EL COMBATE EN:")
+    print("3")
+    time.sleep(1.5)
+    print("2")
+    time.sleep(1.5)
+    print("1")
+    time.sleep(1.5)
+    os.system("cls")
+    while True:
+        # print(comments.comentarios())
+        print(f"{c1} {porcentajeDeC1}% | {vidasDeC1} restantes |")
+        print(f"{c2} {porcentajeDeC2}% | {vidasDeC2} restantes |")
+        if agilizador <=4:
+            agilizador+=1
+
+                
+    
+        
 if __name__ == "__main__":
     main()
