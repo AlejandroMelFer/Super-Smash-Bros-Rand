fc3080a3021b6c5f30d0f221146b073766453fff
MikeSrz <onriosrz@gmail.com>
2025-11-23 11:34:46 +0100

    Mejorando algunas cosas y modificando el json para que sea más descriptivo

--- src/comments.py
+++ src/comments.py
@@ -6,8 +6,8 @@ import random
 import comments
 import percents
 
-def describirAtaque(categoriaAtaque, intensidad, coments):
-    return  random.choice(coments["ataque"][categoriaAtaque].get(intensidad))
+def describirAtaque(categoriasAtaque, intensidad, coments):
+    return  random.choice(coments["ataque"][categoriasAtaque].get(intensidad))
 
 def describirDaño(categoriaAtaque, intensidad, coments):
     return random.choice(coments["recibe"][categoriaAtaque].get(intensidad))
@@ -17,11 +17,11 @@ def narrar(c1,c2,comentarios, categorias):
     golpeador = percents.quienPega(c1, c2)
     golpeado = c1 if golpeador == c2 else c2
     ataque = golpeador.get("ultimoAtaque") #
-    categoriaGolpe = categorias["characters"][golpeador.get("Nombre")]["Move_Set"].get(ataque)
+    categoriasGolpe = categorias["characters"][golpeador.get("Nombre")]["Move_Set"].get(ataque)
     intensidad = categorias["characters"][golpeador.get("Nombre")]["Move_Set"][ataque].get("Intensidad")
 
-    descripcionAtaque = describirAtaque(categoriaGolpe, intensidad, comentarios)
-    descripcionDaño = describirDaño( categoriaGolpe, intensidad, comentarios)
+    descripcionAtaque = describirAtaque(categoriasGolpe, intensidad, comentarios)
+    descripcionDaño = describirDaño( categoriasGolpe, intensidad, comentarios)
 
     #Me falta depurar y añadir más formas de estructurar las frases posibles(Ya continuaré...)
 
@@ -35,6 +35,7 @@ if __name__ == "__main__":
     rutaCategorias = base / ".." / "moveset_categories.json"
     with rutaCategorias.open("r",encoding="utf-8") as categories_json, rutaComentarios.open("r",encoding="utf-8") as comments_json :
         datosCategorias = json.load(categories_json)
-        datosComentarios = json.load(comments_json)
+        comentarios = json.load(comments_json)
 
-    print(datosCategorias["Characters"]["mario"]["Move_Set"]["Golpe_Debil"].get("Intensidad"))
+    print(datosCategorias["Characters"]["mario"]["Move_Set"]["Especial_Arriba_Salto_Puno"].get("Intensidad"))
+    print(f"Mario {random.choice(comentarios["ataque"]["fisico"]["aerea"])}")
